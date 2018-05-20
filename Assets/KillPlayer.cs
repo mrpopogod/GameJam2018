dff709f889dbba496452b3ed8c4e93f6080b7eca
Zach Boyer <zcb5011@gmail.com>
2018-05-20 15:16:21 -0700

    added kill player script for traps / lava
    finished scene 4.Laser Obstacle Course

---
+++ Assets/KillPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour {
+
+
+    public void OnTriggerStay(Collider other)
+    {
+       
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
