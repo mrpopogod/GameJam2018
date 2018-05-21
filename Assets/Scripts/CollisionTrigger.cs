d7c8d48b36fb47d42ddee5088d724d49a5af6925
Zach Boyer <zcb5011@gmail.com>
2018-05-20 21:35:36 -0700

    scene lighting changes

---
+++ Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _receiverObject;
+
+    [SerializeField]
+    private bool isFire;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            return;
+        }
+        if (isFire)
+        {
+            return;
+        }
+ 
+        _receiverObject.GetComponent<ReceiverTrigger>().SendMessage("FireLaser");
+        isFire = true;
+    }
+}
